7526fd6b9df004d4caf1f7cf0abe4fea147bbe68
MRPRO77 <mrpro77@hotmail.com>
2025-06-22 05:03:02 -0300

    Iniciando Configurações Iniciais e desenvolvimento

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,68 @@
-#include <Arduino.h>
+/*******************************************************************************
+   Título:      TCC  Engenharia Mecatrônica.
 
-// put function declarations here:
-int myFunction(int, int);
+   Descrição:   C L P   ---  I H M
+
+   Processador: ARDUINO MEGA ADK +  ROBOCORE ESP32 
+   
+   Geração Multifolhas
+
+
+   Desenvolvido: MRPRO Tecnologia e Automação
+
+   Autor: Tecnologo. Marcelo Rodrigues     Crea-SP 5070505617
+
+   Data de Inicio: 24/09/2023       Versão:   2023.1.0
+
+        
+
+ 
+*******************************************************************************/  
+/* ============================================================================  
+                                                              
+                                       _                      
+                                      / \                     
+                                     |oo >                    
+                                     _\=/_                    
+                    ___         #   /  _  \   #               
+                   /<> \         \\//|/.\|\\//                
+                 _|_____|_        \/  \_/  \/                 
+                | | === | |          |\ /|                    
+                |_|  0  |_|          \_ _/                    
+                 ||  0  ||           | | |                    
+                 ||__*__||           | | |                    
+                |* \___/ *|          []|[]                    
+                /=\ /=\ /=\          | | |                    
+________________[_]_[_]_[_]_________/_]_[_\___________________________________
+                                                                             
+============================================================================== */
+// https://capsistema.com.br/index.php/2020/11/27/como-usar-i2c-no-arduino-comunicacao-entre-duas-placas-arduino/
+
+/******************************************************************************/
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "Wire.h"
+
+
+#include "Config_mcu.h"
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+   
+   Config_mcu();
+  
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+    Comunicacao_Oled();
+
+         comunicacao();
+
+   Supervisionamento(); 
+   
 }
